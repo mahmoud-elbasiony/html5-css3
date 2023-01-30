ad4fd7f8ec190905a7d34e5526797fb68e08ed3c
mahmoud elbasiony <mahmoud.elbasiony904@gmail.com>
2023-01-30 14:24:22 +0200

    add lab03

---
+++ lab03/Worker.js
@@ -0,0 +1,11 @@
+this.onmessage =function(){
+    var summ = 0;
+    for(var i=0;i<1000000000;i++){
+        summ+=i;
+    }
+    // alert(summ)
+    this.postMessage(summ);
+}
+
+//? self === this 
+//& stack - web api - microtasks "promises" ==> event loop
